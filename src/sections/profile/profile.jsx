caf4cbb0bdc9d8080bcdbe5ebcadff3bb36b08b8
Esther Melia Caroline Sigalingging <esthermelia20@gmail.com>
2024-08-06 09:03:19 +0700

    Implement dark and light theme

--- src/sections/profile/profile.jsx
+++ src/sections/profile/profile.jsx
@@ -1,14 +1,24 @@
 import styles from './profileStyles.module.css';
 import profileImg from '../../assets/hero-img.png';
-import themeIcon from '../../assets/sun.svg';
-import twitterIcon from '../../assets/twitter-light.svg';
-import githubIcon from '../../assets/github-light.svg';
-import linkedinIcon from '../../assets/linkedin-light.svg';
-import CV from '../../assets/CV.pdf';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/cv.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Profile() {
     const { theme, toggleTheme } = useTheme();
+
+    const themeIcon = theme === 'light' ? sun : moon;
+    const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+    const githubIcon = theme === 'light' ? githubLight : githubDark;
+    const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
     return (
         <section id="profile" className={styles.container}>
             <div className={styles.colorModeContainer}>
