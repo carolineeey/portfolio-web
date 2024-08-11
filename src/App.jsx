4ea4b7f7d856622c90e5e1926f7e454c392ec3c9
carolineeey <esthermelia20@gmail.com>
2024-08-11 10:07:31 +0700

    Add project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
 import Profile from './sections/profile/profile';
+import Projects from './sections/projects/projects'
 
 function App() {
   return (
-    <> <Profile />  </>
+    <> 
+      <Profile />  
+      <Projects /> 
+    </>
   );
 }
 
