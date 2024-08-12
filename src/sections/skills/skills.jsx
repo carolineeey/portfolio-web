df4d33ff45eb3bc50b25b01f41cba6dbffb8b002
carolineeey <esthermelia20@gmail.com>
2024-08-12 08:47:59 +0700

    Missing implementation of icon theme

--- src/sections/skills/skills.jsx
+++ src/sections/skills/skills.jsx
@@ -1,8 +1,13 @@
 import styles from './skillsStyles.module.css'
-import checkMarkIcon from '../../assets/checkmark-dark.svg'
+import checkMarkIconDark from '../../assets/checkmark-dark.svg'
+import checkMarkIconLight from '../../assets/checkmark-light.svg'
 import SkillList from '../../common/SkillList'
+import { useTheme } from '../../common/ThemeContext';
 
 export default function skills() {
+  const { theme, toggleTheme } = useTheme();
+  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+
   return (
     <section id="skills" className={styles.container}>
         <h1 className="sectionTitle">Skills</h1>
